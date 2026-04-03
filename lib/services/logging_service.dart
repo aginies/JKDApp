e965cc5617c627f2164c5c8f93c32c2a137c7e06
aginies <aginies@suse.com>
2026-04-03 12:25:43 +0200

    update version

--- lib/services/logging_service.dart
+++ lib/services/logging_service.dart
@@ -5,7 +5,7 @@ import 'package:file_picker/file_picker.dart';
 
 class LoggingService {
   static final List<String> _logs = [];
-  static const String appVersion = "1.7.1+1";
+  static const String appVersion = "1.8.0";
 
   static void log(String message) {
     final timestamp = DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
