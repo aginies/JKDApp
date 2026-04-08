66e4572056cf33710687cac964e3aa4bfe969cdf
aginies <aginies@suse.com>
2026-04-08 23:57:31 +0200

    bug fixes and performance improvment

--- lib/screens/series_detail/widgets/move_list_display_widget.dart
+++ lib/screens/series_detail/widgets/move_list_display_widget.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 import '../../../services/localization_service.dart';
 import '../controllers/training_controller.dart';
 import 'move_display_widgets.dart';
+import 'diagonal_cross.dart';
 
 /// Displays the list of moves in a series with edit controls
 class MoveListDisplayWidget {
@@ -1010,52 +1011,3 @@ class MoveListDisplayWidget {
   }
 }
 
-class DiagonalCross extends StatelessWidget {
-  final Widget child;
-  final bool show;
-  final Color color;
-
-  const DiagonalCross({
-    super.key,
-    required this.child,
-    required this.show,
-    this.color = Colors.purple,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    if (!show) return child;
-
-    return Stack(
-      children: [
-        child,
-        Positioned.fill(
-          child: IgnorePointer(
-            child: CustomPaint(painter: DiagonalCrossPainter(color: color)),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class DiagonalCrossPainter extends CustomPainter {
-  final Color color;
-
-  DiagonalCrossPainter({required this.color});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = color.withValues(alpha: 0.5)
-      ..strokeWidth = 2.5
-      ..strokeCap = StrokeCap.round;
-
-    // Draw X
-    canvas.drawLine(const Offset(0, 0), Offset(size.width, size.height), paint);
-    canvas.drawLine(Offset(size.width, 0), Offset(0, size.height), paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-}
