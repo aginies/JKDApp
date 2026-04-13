eea5d9b9166382458544f6f8e32c01cec447cb0c
aginies <aginies@suse.com>
2026-04-13 16:11:04 +0200

    bump version

--- lib/services/logging_service.dart
+++ lib/services/logging_service.dart
@@ -8,7 +8,7 @@ enum LogLevel { info, warn, error, debug }
 
 class LoggingService {
   static final List<String> _logs = [];
-  static const String appVersion = "2.3.0";
+  static const String appVersion = "2.4.0";
 
   /// Standard log (defaults to INFO)
   static void log(String message) {
